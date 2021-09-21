76ca13176bd144e685842241f9a2fcdd45c78be6
Timothée <tnguyen@normandiewebschool.fr>
2021-09-21 15:01:05 +0200

    Test CheckFields validé

--- SkromNWSRPG/Item.cs
+++ SkromNWSRPG/Item.cs
@@ -11,7 +11,13 @@ namespace SkromNWSRPG
      * Il porte un Name
      * C'est une classe Abstraite
      */
-    public class Item
+    public abstract class Item
     {
+        public string Name;
+
+        public Item(string name)
+        {
+            Name = name;
+        }
     }
 }
